f584182bebfb4af928fd4cf7fac28b664394c492
lars34932 <34932@ma-web.nl>
2024-08-16 17:02:27 +0200

    begin home page

--- public/index.php
+++ public/index.php
@@ -4,10 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/6c294d23a1.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.typekit.net/vmz6jnb.css">
+    <link rel="stylesheet" href="https://use.typekit.net/vmz6jnb.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Portfolio</title>
 </head>
 <body>
-    
+    <header class="header">
+        <img src="assets/img/logo.webp" alt="" class="header__logo">
+        <div class="header__div">
+            <a href="" class="header__link">projects</a>
+            <a href="" class="header__link">about me</a>
+            <a href="" class="header__link">contact</a>
+        </div>
+    </header>
+    <main class="main">
+        <section class="main__intro">
+            <div class="main__div main__div1">
+                <div>
+                <h1 class="main__title">Backend developer +</h1>
+                <h1 class="main__title">Web developer</h1>
+                </div>
+                <p class="main__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates impedit unde doloribus voluptatibus dignissimos accusantium rerum rem. Est itaque eligendi officia quos voluptatem incidunt eveniet reiciendis mollitia, praesentium quam repellat?</p>
+                <button class="main__button">Download CV  <i class="fa-solid fa-download"></i></button>
+            </div>
+            <div class="main__div">
+                <img src="" alt="" class="main__img">
+            </div>
+        </section>
+        <section class="main__projects">
+            <div class="main__divider">Projects</div>
+            <ul class="projects__cards">
+                <li class="projects__card">
+                    <button class="project__button"></button>
+                </li>
+                <li class="projects__card"></li>
+                <li class="projects__card"></li>
+                <li class="projects__card"></li>
+            </ul>
+        </section>
+    </main>
+    <footer class="footer">
+
+    </footer>
 </body>
 </html>
