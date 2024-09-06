6b427d7f9006dfe37453d3e34e5b20d2b15949c0
lars34932 <34932@ma-web.nl>
2024-09-06 17:23:39 +0200

    klein begin project page

--- public/projectPage.php
+++ public/projectPage.php
@@ -12,23 +12,54 @@ require_once("/var/www/html/source/connect.php");
     <link rel="stylesheet" href="https://use.typekit.net/vmz6jnb.css">
     <link rel="stylesheet" href="https://use.typekit.net/vmz6jnb.css">
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/style-index.css">
+    <link rel="stylesheet" href="assets/css/style-page.css">
     <title>Portfolio</title>
 </head>
 <body>
     <?php makeHeader();?>
+    <main class="main">
+        <img class="main__img" src="assets/img/test.png" alt="">
+        <section class="info__section">
+            <div class="info__div">
+                <h1 class="info__title">Titel</h1>
+                <p class="info__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus cupiditate optio illo nisi neque rem qui obcaecati iusto dolorum earum consequuntur inventore aperiam quia fuga, labore vero numquam mollitia cumque?</p>
+                
+            </div>
+            <ul class="info__list">
+                <li class="list__item">
+                    <p class="item__title">test</p>
+                    <p class="item__text">test</p>
+                </li>
+                <li class="list__item">
+                    <p class="item__title">test</p>
+                    <p class="item__text">test</p>
+                </li>
+                <li class="list__item">
+                    <p class="item__title">test</p>
+                    <p class="item__text">test</p>
+                </li>
+                <li class="list__item">
+                    <p class="item__title">test</p>
+                    <p class="item__text">test</p>
+                </li>
+            </ul>
+        </section>
+        <button class="live__button">Live preview</button>
+        <h2 class="about__title">beschrijving</h2>
+        <p class="about__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sint amet perspiciatis incidunt. Sit eos laborum voluptatum inventore veniam ipsam quibusdam sunt corporis ex reprehenderit temporibus assumenda autem, harum at?</p>
+    </main>
     <?php 
-    $name = isset($_GET['name']) ? $_GET['name'] : '';
+    // $name = isset($_GET['name']) ? $_GET['name'] : '';
 
-    if (!empty($name)) {
-        $pdo = db_connect();
-        $stmt = $pdo->prepare('SELECT * FROM Projects WHERE name = ?');
-        $stmt->execute([$name]);
-        $project = $stmt->fetch(PDO::FETCH_ASSOC);
-        echo $project['name'];
-    } else {
-        echo '<p>Project could not be found.</p>';
-    }
+    // if (!empty($name)) {
+    //     $pdo = db_connect();
+    //     $stmt = $pdo->prepare('SELECT * FROM Projects WHERE name = ?');
+    //     $stmt->execute([$name]);
+    //     $project = $stmt->fetch(PDO::FETCH_ASSOC);
+    //     echo $project['name'];
+    // } else {
+    //     echo '<p>Project could not be found.</p>';
+    //}
     ?>
     <?php makeFooter();?>
 </body>
