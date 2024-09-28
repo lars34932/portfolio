f56adbf6ae2cd95e000d2d3a1b4a6933b4547c5f
lars34932 <34932@ma-web.nl>
2024-09-28 17:19:31 +0200

    kleine styling

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -2,10 +2,8 @@ const menuElement = document.getElementById("js--menu");
 
 function menu() {
     menuElement.style.display = "flex"
-    document.body.style.overflow = 'hidden';
 }
 
 function noMenu() {
     menuElement.style.display = "none"
-    document.body.style.overflow = '';
 }
